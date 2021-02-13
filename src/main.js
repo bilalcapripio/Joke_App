178f09292d61a07c341d76735decc6db4800623e
bilal <bilal.rizwaan1909@gmail.com>
2021-02-13 15:45:13 +0500

    Jokes App

--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
-import Vue from 'vue'
+import { createApp } from 'vue'
 import App from './App.vue'
-// import Vuex from 'Vuex'
+import Vue from 'vue'
+import Vuex from 'Vuex'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import VueRouter from 'vue-router'
 import Home from './components/Home'
 import About from './components/About'
-// Vue.use(Vuex)
-// Vue.use(VueRouter);
+Vue.use(Vuex)
+Vue.use(VueRouter);
 const routes=[
     {path:'/',component:Home},
     {path:'/About',component:About}
@@ -14,9 +15,4 @@ const routes=[
 const router=new VueRouter({
     routes
 })
-
-
-new Vue({
-    router:router,
-    render:h=>h(App),
-}).$mount('#app')
+createApp(App).mount('#app')
